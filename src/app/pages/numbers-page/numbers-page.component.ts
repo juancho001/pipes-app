5df77b0e242a863b498f72884e224cd8d55aaa88
Juan Carlos Avila Meza <avilameza@gmail.com>
2025-04-21 19:57:59 -0500

    Trabajando con Pipes de Angular basados en Números

--- src/app/pages/numbers-page/numbers-page.component.ts
+++ src/app/pages/numbers-page/numbers-page.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-numbers-page',
-  imports: [],
+  imports: [DecimalPipe,PercentPipe, CurrencyPipe ],
   templateUrl: './numbers-page.component.html',
 })
 export default class NumbersPageComponent {
+
+  totalSells = signal(2_433_232.5567)
+  porcent = signal(0.4856)
 
 }
